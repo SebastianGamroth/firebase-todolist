285ae823b1906b3b0fcae03e69fd22e2f3041f95
user7d11 <user7w11@gmail.com>
2022-07-25 12:50:54 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-todolist';
+  // variable todos$ = $ damit wir wissen, dass die Variable sich immer updated
+  // variable todos$ bekommt eine variable Observable (die sich imer updated)
+  // any = json
+  todos$: Observable<any>;
+  // importieren Firestore und weisen der Variable firestore: zu
+  constructor(firestore: Firestore) {
+    // die collection aus firesotre die todos
+    const coll = collection(firestore, 'todos');
+    // mit collectionData hollen wir die Daten aus coll
+    this.todos$ = collectionData(coll);
+
+    /**
+     * Die Funktion wird jedes aufgerufen wenn sich die Daten in der Datenbank ändern 
+     */
+    this.todos$.subscribe((newTodos) => {
+      console.log(newTodos);
+    });
+
+  }
 }
